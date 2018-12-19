cf6205c570ea1ee3ddd34c0828d43b3df9cffb14
Joseph Tandingan <josephdevapps@gmail.com>
2018-12-20 03:30:01 +0800

    Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +11,21 @@ export class HeroesComponent implements OnInit {
   // hero = 'Windstorm';
   // Refactor property to of type Hero and initialize it with an id of 1
   // Changed from a string to an object
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  // define a component properly called heroes to expose
+  // HEROES array for binding.
+  heroes = HEROES;
+
+  // method which assigns the clicked her from the template
+  // to the component's selectedHero.
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
